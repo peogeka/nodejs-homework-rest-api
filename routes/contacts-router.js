d7565dca5fcaabbc58f2e2a1576b708c532da383
peogeka <peo.geka@gmail.com>
2023-12-15 12:22:04 +0100

    for verification-1

--- routes/contacts-router.js
+++ routes/contacts-router.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import contactController from "../../controllers/controller.js";
-import {authentication, isEmptyBody, isValidId} from '../middlewares/index.js';
-import {bodyValidator} from '../decorators/index.js';
-import { contactFavoriteSchema, contactsAddSchema, contactsUpdateSchema } from '../models/Contact.js';
+import contactController from "../../controllers/contacts-controller.js";
+import {authentication, isEmptyBody, isValidId} from '../../middlewares/index.js';
+import {bodyValidator} from '../../decorators/index.js';
+import { contactFavoriteSchema, contactsAddSchema, contactsUpdateSchema } from '../../models/Contact.js';
 
 const router = express.Router();
 
@@ -19,6 +19,8 @@ router.route('/:id')
    .delete(isValidId, contactController.deleteById);
 
 router.route('/:id/favorite')
-   .patch(isValidId, isEmptyBody, bodyValidator(contactFavoriteSchema), contactController.updateFavoriteById)
+   .patch(isValidId, isEmptyBody, bodyValidator(contactFavoriteSchema), contactController.updateFavoriteById);
 
-export default router
+
+
+export default router;
